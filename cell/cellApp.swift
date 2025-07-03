d003592b889a386660607e360d249a5059985512
francescafinetti <francy.fine@icloud.com>
2025-07-03 11:39:07 +0200

    Initial Commit

---
+++ cell/cellApp.swift
@@ -0,0 +1,17 @@
+//
+//  cellApp.swift
+//  cell
+//
+//  Created by Francesca Finetti on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct cellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
